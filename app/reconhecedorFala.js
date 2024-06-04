f5a68b672d7c9589eba83ffe1f6c1d067dcb5e9d
Jean Carlos <jeanxh4@gmail.com>
2024-06-04 20:18:34 -0300

    validação de números.

--- app/reconhecedorFala.js
+++ app/reconhecedorFala.js
@@ -1,9 +1,17 @@
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+var numeroFalado = document.querySelector('.box');
 
 const recognition = new SpeechRecognition();
 recognition.lang = "pt-Br";
 recognition.start();
 
-recognition.addEventListener(() =>{
-    console.log()
-});
+recognition.addEventListener('result', Speak)
+
+function Speak(e){
+    if (/^\d+$/.test(e.results[0][0].transcript)) {
+        numeroFalado.innerHTML = e.results[0][0].transcript;
+        // Aqui você pode fazer algo com a entrada numérica
+      } else {
+        console.log('Entrada inválida, por favor fale apenas números.');
+      }
+}
